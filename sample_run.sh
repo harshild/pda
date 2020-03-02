809fb707730d82983844bc6d72f9a1753a1cd32c
Meera <meera7419@gmail.com>
2020-03-02 10:35:23 -0500

    sample PDAs added

--- sample_run.sh
+++ sample_run.sh
@@ -1,1 +1,3 @@
-go build ./main/driver.go && ./driver ./sample/samplePDA-1 0011
+go build ./main/driver.go && ./driver ./sample/samplePDA-1.json 0011
+go build ./main/driver.go && ./driver ./sample/samplePDA-2.json aabcc
+go build ./main/driver.go && ./driver ./sample/samplePDA-3.json 0101220101
