c802e73f92cffadc1c1589a5ff9ba216dcbcb4f6
Meera <meera7419@gmail.com>
2020-05-18 16:46:25 -0400

    Check for pda id in pda status to make sure it's connected

--- src/controller/pda_controller.go
+++ src/controller/pda_controller.go
@@ -54,25 +54,30 @@ func (pdaController *PdaController) PutsToken(writer http.ResponseWriter, reques
 	pda_id := params["id"]
 
 	pdaStatus := pdaController.getPDAStatus(request)
+	if pdaStatus.PdaId == pda_id {
 
-	all, _ := ioutil.ReadAll(request.Body)
-	token := string(all)
-	position, _ := strconv.Atoi(params["position"])
+		all, _ := ioutil.ReadAll(request.Body)
+		token := string(all)
+		position, _ := strconv.Atoi(params["position"])
 
-	//call manager to pass a token with position
-	err := pdaController.PdaManager.Puts(pda_id, token, position, pdaStatus)
+		//call manager to pass a token with position
+		err := pdaController.PdaManager.Puts(pda_id, token, position, pdaStatus)
 
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+		}
+		cookieDetails := pdaController.PdaManager.GetCookieFor(pdaStatus.ReplicaId, pdaStatus.PdaId)
+		val, _ := json.Marshal(cookieDetails)
+
+		cookie := &http.Cookie{
+			Name:  "pda",
+			Value: strings.ReplaceAll(string(val), "\"", "'"),
+		}
+		http.SetCookie(writer, cookie)
+	} else {
+		http.Error(writer, "Connect to the PDA first!! then send token", 501)
 	}
-	cookieDetails := pdaController.PdaManager.GetCookieFor(pdaStatus.ReplicaId, pdaStatus.PdaId)
-	val, _ := json.Marshal(cookieDetails)
 
-	cookie := &http.Cookie{
-		Name:  "pda",
-		Value: strings.ReplaceAll(string(val), "\"", "'"),
-	}
-	http.SetCookie(writer, cookie)
 }
 
 func (pdaController *PdaController) getPDAStatus(request *http.Request) entity.PDAStatus {
@@ -103,20 +108,23 @@ func (pdaController *PdaController) PutsEOS(writer http.ResponseWriter, request 
 	//call manager to call eos for pda_id, ignore position of eos
 	pdaStatus := pdaController.getPDAStatus(request)
 
-	err := pdaController.PdaManager.PutsEOS(pda_id, position, pdaStatus)
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	cookieDetails := pdaController.PdaManager.GetCookieFor(pdaStatus.ReplicaId, pdaStatus.PdaId)
-	val, _ := json.Marshal(cookieDetails)
+	if pdaStatus.PdaId == pda_id {
+		err := pdaController.PdaManager.PutsEOS(pda_id, position, pdaStatus)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		cookieDetails := pdaController.PdaManager.GetCookieFor(pdaStatus.ReplicaId, pdaStatus.PdaId)
+		val, _ := json.Marshal(cookieDetails)
 
-	cookie := &http.Cookie{
-		Name:  "pda",
-		Value: strings.ReplaceAll(string(val), "\"", "'"),
+		cookie := &http.Cookie{
+			Name:  "pda",
+			Value: strings.ReplaceAll(string(val), "\"", "'"),
+		}
+		http.SetCookie(writer, cookie)
+	} else {
+		http.Error(writer, "Connect to the PDA first,then send EOS", 502)
 	}
-	http.SetCookie(writer, cookie)
-
 }
 
 func (pdaController *PdaController) IsPDAAccepted(writer http.ResponseWriter, request *http.Request) {
@@ -125,14 +133,18 @@ func (pdaController *PdaController) IsPDAAccepted(writer http.ResponseWriter, re
 	//call manager to call is_accepted method for pda_id
 	pdaStatus := pdaController.getPDAStatus(request)
 
-	is_accepted, err := pdaController.PdaManager.Is_accepted(pda_id, pdaStatus)
+	if pdaStatus.PdaId == pda_id {
+		is_accepted, err := pdaController.PdaManager.Is_accepted(pda_id, pdaStatus)
 
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		accepted, err := json.Marshal(is_accepted)
+		writer.Write(accepted)
+	} else {
+		http.Error(writer, "connect to the PDA first, then call is_accepted ", 503)
 	}
-	accepted, err := json.Marshal(is_accepted)
-	writer.Write(accepted)
 }
 
 func (pdaController *PdaController) PeekStack(writer http.ResponseWriter, request *http.Request) {
@@ -142,13 +154,17 @@ func (pdaController *PdaController) PeekStack(writer http.ResponseWriter, reques
 	//call manager to call peek method for pda_id and position peek_k from top
 	pdaStatus := pdaController.getPDAStatus(request)
 
-	top_k, err := pdaController.PdaManager.Peek(pda_id, peek_k, pdaStatus)
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+	if pdaStatus.PdaId == pda_id {
+		top_k, err := pdaController.PdaManager.Peek(pda_id, peek_k, pdaStatus)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		top_kjson, _ := json.Marshal(top_k)
+		writer.Write(top_kjson)
+	} else {
+		http.Error(writer, "First,Connect to the member of Replica group! ", 504)
 	}
-	top_kjson, _ := json.Marshal(top_k)
-	writer.Write(top_kjson)
 }
 
 func (pdaController *PdaController) StackSize(writer http.ResponseWriter, request *http.Request) {
@@ -157,13 +173,17 @@ func (pdaController *PdaController) StackSize(writer http.ResponseWriter, reques
 	//call manager to return the value of stack.Size() for pda_id
 	pdaStatus := pdaController.getPDAStatus(request)
 
-	size, err := pdaController.PdaManager.Size(pda_id, pdaStatus)
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+	if pdaStatus.PdaId == pda_id {
+		size, err := pdaController.PdaManager.Size(pda_id, pdaStatus)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		stacksize, _ := json.Marshal(size)
+		writer.Write(stacksize)
+	} else {
+		http.Error(writer, "First,Connect to the member of Replica group! ", 505)
 	}
-	stacksize, _ := json.Marshal(size)
-	writer.Write(stacksize)
 }
 
 func (pdaController *PdaController) CurrentStatePDA(writer http.ResponseWriter, request *http.Request) {
@@ -172,13 +192,17 @@ func (pdaController *PdaController) CurrentStatePDA(writer http.ResponseWriter, 
 	//call manager to return the value of pdaprocessor.current_state() for pda_id
 	pdaStatus := pdaController.getPDAStatus(request)
 
-	state, err := pdaController.PdaManager.Currentstate(pda_id, pdaStatus)
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+	if pdaStatus.PdaId == pda_id {
+		state, err := pdaController.PdaManager.Currentstate(pda_id, pdaStatus)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		currentstate, _ := json.Marshal(state)
+		writer.Write(currentstate)
+	} else {
+		http.Error(writer, "First,Connect to the member of Replica group! ", 506)
 	}
-	currentstate, _ := json.Marshal(state)
-	writer.Write(currentstate)
 }
 
 func (pdaController *PdaController) QueuedTokenPDA(writer http.ResponseWriter, request *http.Request) {
@@ -187,13 +211,18 @@ func (pdaController *PdaController) QueuedTokenPDA(writer http.ResponseWriter, r
 	//call manager to return the value of queued tokens( that method id yet to be implemented) for pda_id
 	pdaStatus := pdaController.getPDAStatus(request)
 
-	token, err := pdaController.PdaManager.Queued_token(pda_id, pdaStatus)
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+	if pdaStatus.PdaId == pda_id {
+		token, err := pdaController.PdaManager.Queued_token(pda_id, pdaStatus)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		q_token, _ := json.Marshal(token)
+		writer.Write(q_token)
+	} else {
+		http.Error(writer, "First,Connect to the member of Replica group! ", 507)
 	}
-	q_token, _ := json.Marshal(token)
-	writer.Write(q_token)
+
 }
 func (pdaController *PdaController) SnapshotPDA(writer http.ResponseWriter, request *http.Request) {
 	params := mux.Vars(request)
@@ -202,31 +231,36 @@ func (pdaController *PdaController) SnapshotPDA(writer http.ResponseWriter, requ
 	//call manager to call three methods for pda_id: pdaprocessor.current_state(), queued tokens, peek(peek_k)
 	pdaStatus := pdaController.getPDAStatus(request)
 
-	state, err := pdaController.PdaManager.Currentstate(pda_id, pdaStatus)
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+	if pdaStatus.PdaId == pda_id {
+		state, err := pdaController.PdaManager.Currentstate(pda_id, pdaStatus)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		token, err := pdaController.PdaManager.Queued_token(pda_id, pdaStatus)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		peek, err := pdaController.PdaManager.Peek(pda_id, peek_k, pdaStatus)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		out := make([]string, 0)
+
+		out = append(out, state)
+		out = append(out, strings.Join(token, ","))
+		out = append(out, strings.Join(peek, " "))
+
+		json_out, _ := json.Marshal(out)
+
+		writer.Write(json_out)
+	} else {
+		http.Error(writer, "First,Connect to the member of Replica group! ", 508)
 	}
-	token, err := pdaController.PdaManager.Queued_token(pda_id, pdaStatus)
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	peek, err := pdaController.PdaManager.Peek(pda_id, peek_k, pdaStatus)
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
-	}
 
-	out := make([]string, 0)
-
-	out = append(out, state)
-	out = append(out, strings.Join(token, ","))
-	out = append(out, strings.Join(peek, " "))
-
-	json_out, _ := json.Marshal(out)
-
-	writer.Write(json_out)
 }
 func (pdaController *PdaController) ClosePDA(writer http.ResponseWriter, request *http.Request) {
 	params := mux.Vars(request)
